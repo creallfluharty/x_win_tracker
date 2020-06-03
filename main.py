716d790ec82ad2b773719e03ac01d7657b24b706
Christian Reall Fluharty <creallfluharty@gmail.com>
2020-06-03 00:37:31 +0000

    create main func

--- main.py
+++ main.py
@@ -3,19 +3,21 @@ from sqlalchemy.orm import sessionmaker
 
 from orm.activity_manager_object_manager_builder import ActivityManagerObjectManagerBuilder
 
-if __name__ == '__main__':
 
-    object_manager_builder = ActivityManagerObjectManagerBuilder()
+def main():
     engine = create_engine('sqlite:///a.db')
-
     Session = sessionmaker(bind=engine)
     session = Session()
 
+    object_manager_builder = ActivityManagerObjectManagerBuilder()
     object_manager = object_manager_builder.build(session)
 
     activities = session.query(object_manager.get_object('Activity')).all()
     print(activities)
 
+
+if __name__ == '__main__':
+    main()
     # application = Application(icon_name='asdf')
     # session.add(application)
     # session.flush()
